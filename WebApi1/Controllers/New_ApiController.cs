d5afc5b6a74eb6b9357c2f693e0c71752d046f1e
akrishnakumar <akrishnakumar@SUYPC227>
2017-09-07 14:31:45 +0530

    Add project files.

---
+++ WebApi1/Controllers/New_ApiController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebApi1.Controllers
+{    
+    public class New_ApiController : ApiController
+    { int i = 5;
+        [HttpPost][Route("den")]
+        public int Display()
+        {
+            return 2;
+        }
+        [HttpGet]
+        public string MyName(string str)
+        {
+            //str = "athira";
+            return str;
+        }
+       [HttpPut][Route("ping")]
+       public int change(int i)
+        {
+            i = 3;
+            return i;
+        }
+        [HttpDelete]
+        [Route("pop")]
+        public string Pop(int i)
+        {
+           
+            return "hello";
+        }
+    }
+}
